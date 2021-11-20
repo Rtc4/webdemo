1d459f7d9973975600a887bd3440aa6260b384ee
Rtc4 <924029106@qq.com>
2021-11-20 13:18:31 +0800

    version1.0

--- src/main/java/com/tan/ServletDispatcher.java
+++ src/main/java/com/tan/ServletDispatcher.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/dispatcher")
+@WebServlet(name = "dispatcher",urlPatterns = {"/dispatcher"})
 public class ServletDispatcher extends HttpServlet {
     @Override
 
